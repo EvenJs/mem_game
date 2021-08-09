6ae85aae99b2a3f96670dea237980c0911a51d2c
EvenW <yuhang0229@gmail.com>
2021-08-09 16:00:54 +1000

    update

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ const CARD_TECHS = [
   'linkedin',
   'heroku',
   'github',
+  'aws',
+  'html5',
+  'css3',
+  'js',
+  'sass',
+  'nodejs',
+  'react',
+  'linkedin',
+  'heroku',
+  'github',
   'aws'
 ];
 
@@ -18,7 +28,7 @@ const game = {
   score: 0,
   level: 1,
   timer: 60,
-  timerDisplay: document.querySelector('.game-timer__bar'),
+  timerDisplay:  document.querySelector('.game-timer__bar'),
   scoreDisplay: document.querySelector('.game-stats__score--value'),
   levelDisplay: document.querySelector('.game-stats__level--value'),
   timerInterval: null,
@@ -27,272 +37,239 @@ const game = {
   gameBoard: document.querySelector('.game-board'),
   cardFlipNum: 0,
   cardPairNum: 0,
-  
 };
 
-
-
 setGame();
-
-//handleCardFlip() 
 
 /*******************************************
 /     game process
 /******************************************/
-
 function setGame() {
-  // register any element in your game object  
- startGame();
+  // register any element in your game object
+  let {startButton} = game;
 
+  startButton.addEventListener('click',() =>{
+    if (startButton.innerHTML === 'End Game'){
+   
+      clearInterval(game.timerInterval);
+      //handleGameOver();
+      GameOver();
+      startButton.innerHTML = 'Start Game';
+      
+      console.log(1,startButton.innerHTML )
+   }else if(startButton.innerHTML === 'New Game'){
+      startButton.innerHTML = 'End Game';
+      updateTimerDisplay();
+      firstLevel();
+      handleCardFlip();
+   }else{
+        //updateTimerDisplay();
+        startButton.innerHTML = 'End Game';
+        startGame();
+
+        console.log(2,startButton.innerHTML )
+       // handleCardFlip();
+       
+     }
+  })
+}
+
+function startGame() {
+  let card = document.querySelectorAll('.card');
+  card.forEach(card => {
+    card.classList.remove('done');
+  });
+  const { startButton } = game;
+  clearInterval(game.timerInterval);
+  //startButton.innerHTML = 'Start Game';
+  setGame();
+  resetGame();
+  firstLevel();
+  handleCardFlip();
+}
+
+function handleCardFlip() {
+  let cards = document.querySelectorAll('.card');
+  let fliped = [];
+  let {cardFlipNum} = game;
+  
+  cards.forEach(card=>{
+    card.addEventListener('click',()=>{
+      if(cardFlipNum === 0 && !card.classList.contains('done')) {
+        card.classList.add('card--flipped','done');
+        fliped[cardFlipNum] = card;
+        console.log(cardFlipNum);
+        cardFlipNum = 1;
+
+      }
+      if(cardFlipNum ===1 && !card.classList.contains('done')){
+        card.classList.add('card--flipped','done');
+        fliped[cardFlipNum] = card;
+        console.log(cardFlipNum);
+        cardFlipNum = 2;
+        
+        // check two card same or not by data-tech 
+        if(fliped[0].dataset.tech === fliped[1].dataset.tech)
+        {
+          game.cardPairNum +=1;
+          updateScore();
+          passLevel();
+          cardFlipNum = 0;
+
+        }else {
+          setTimeout(()=>{ 
+            fliped[0].classList.remove('done','card--flipped');
+            fliped[1].classList.remove('done','card--flipped');
+            cardFlipNum = 0;
+          },1500)
+        }
+      }  
+    })
+  })
 }
 
 
+function passLevel(){
+  if(game.level === 1 && game.cardPairNum === 2){
+    setTimeout(()=>{
+      secondLevel();
+      updateLevel();
+      handleCardFlip();
+    },1500)
+  }else if(game.level ===2 && game.cardPairNum === 8){
+    setTimeout(()=>{
+      thirdLevel();
+      updateLevel();
+      handleCardFlip();
+    },1500)
+  }
+  else if(game.level === 3 && game.cardPairNum === 18){
+    handleGameOver();
+  }
+}
 
-function startGame() {
-  game.startButton.addEventListener('click',function(){
-    if(game.startButton.innerHTML === 'New Game'){
-      firstLevel();
-      //secondLevel();
-      //handleCardFlip();
-      levelPass();
-      game.startButton.innerHTML = 'End Game';
-     
+function nextLevel() {}
 
-    }
-
-    else if(game.startButton.innerHTML === 'End Game')
-    {
-  
-      //seperate for game over or timeout
-      alert('Congratulations, your score is'+ game.score);
-      game.startButton.innerHTML = 'New Game';
-      
-      clearInterval(game.timerInterval);
-      
-
-    }
-    else {
-      game.startButton.innerHTML = 'Start Game';
-    }
-  })
+function GameOver() {
+  alert('Congratulations, your score is')
+  game.level = 1;
+  game.score = 0;
+  resetGame();
+  let card = document.querySelectorAll('.card');
+  card.forEach(card => {
+    card.classList.add('done');
+  });
 }
 
 function createCards(className){
   let newCard = document.createElement('div');
+  let newCardFront = document.createElement('div');
+  let newCardBack = document.createElement('div');
+
   newCard.classList.add('card',`${className}`);
   newCard.setAttribute('data-tech',`${className}`);
-  
-  let newCardFront = document.createElement('div');
   newCardFront.classList.add('card__face','card__face--front');
- 
-  let newCardBack = document.createElement('div');
   newCardBack.classList.add('card__face','card__face--back');
-  
+
   newCard.append(newCardFront,newCardBack);
   return newCard;
 }
 
 function firstLevel() {
-  const { gameBoard } = game;
-  gameBoard.style = 'grid-template-columns: repeat(2, 1fr)' ;
-  gameBoard.innerHTML = null;       
-  gameBoard.append(
-    createCards('css3'),
-    createCards('html5'),
-  )
+  const {gameBoard} = game;
+  gameBoard.style = 'grid-template-columns: repeat(2, 1fr)';
+  gameBoard.innerHTML = null;
 
+  for(let i = 0; i < 2; i ++){
+  gameBoard.append(createCards(CARD_TECHS[i]))
+  }
   createCardCopy();
   shuffle();
-  updateTimerDisplay();
+
 }
 
 function secondLevel(){
   const {gameBoard} = game;
   gameBoard.style = 'grid-template-columns: repeat(4, 1fr)';
   gameBoard.innerHTML = null;
-  gameBoard.append(
-  
-    createCards('js'),
-    createCards('sass'),
-    createCards('nodejs'),
-    createCards('react'),
-    createCards('linkedin'),
-    createCards('heroku'),
-    createCards('github'),
-    createCards('aws'),
-  )
-  createCardCopy();
- // shuffle();
-}
 
-function thirdLevel(){
-  const {gameBoard} = game;
-  gameBoard.style = 'grid-template-columns: repeat(6, 1fr);';
-  gameBoard.innerHTML = null;
-  gameBoard.append(
-    createCards('js'),
-    createCards('sass'),
-    createCards('nodejs'),
-    createCards('react'),
-    createCards('linkedin'),
-    createCards('heroku'),
-    createCards('github'),
-    createCards('aws'),
-    createCards('css3'),
-    createCards('html5'),
-    createCards('js'),
-    createCards('sass'),
-    createCards('nodejs'),
-    createCards('react'),
-    createCards('linkedin'),
-    createCards('heroku'),
-    createCards('github'),
-    createCards('aws'),
-
-  )
+  for(let i = 0; i < 8; i ++){
+    gameBoard.append(createCards(CARD_TECHS[i]))
+    }
   createCardCopy();
   shuffle();
 }
 
+function thirdLevel() {
+  const {gameBoard} = game;
+  gameBoard.style = 'grid-template-columns: repeat(6, 1fr)';
+  gameBoard.innerHTML = null;
+
+  for(let i = 0; i < 18; i ++){
+    gameBoard.append(createCards(CARD_TECHS[i]))
+    }
+  
+  createCardCopy();
+  shuffle();
+}
 
 function createCardCopy() {
-  const { gameBoard } = game;
+  const {gameBoard} = game;
+
   gameBoard.childNodes.forEach(
-    (card) => {
-      cardClone = card.cloneNode(true);
-      gameBoard.append(cardClone);
+    card =>{
+      cardCopy = card.cloneNode(true);
+      gameBoard.append(cardCopy);
     }
   )
 }
-
-function handleCardFlip() {
-  let fliped = {};
-  let card = document.querySelectorAll('.card');
-
-    card.forEach((card)=>{
- 
-      card.addEventListener('click', function(){
-       
-        if(game.cardFlipNum === 0 && !card.classList.contains('done')){
-          card.classList.add('card--flipped','done');
-          game.cardFlipNum = 1;
-          fliped.card1 = card;
-          //console.log(game.cardFlipNum);
-          //console.log(1,fliped.card1);    
-        }else if (game.cardFlipNum === 1 && !card.classList.contains('done')){
-          card.classList.add('card--flipped');
-          game.cardFlipNum = 2;
-          fliped.card2 = card;
-          //console.log(game.cardFlipNum);
-          //console.log(2, fliped.card1.classList[1]);
-          //console.log(3, fliped.card2);
-          if(fliped.card1.classList[1] === fliped.card2.classList[1])
-          {
-            //update spore
-            fliped.card1.classList.add('done');
-            fliped.card2.classList.add('done');
-            updateScore();
-            game.cardFlipNum = 0;
-            game.cardPairNum += 1;
-            console.log(game.cardPairNum);
-            nextLevel();
-          } else{  
-            setTimeout(()=>{
-              fliped.card1.classList.remove('card--flipped','done');
-              fliped.card2.classList.remove('card--flipped');
-              game.cardFlipNum = 0;
-            },1000)
-            
-          }
-        }
-      })
-    })
-}
-
-function nextLevel(){
-  if(game.level === 1 && game.cardPairNum === 2){
-    setTimeout(()=>{
-      //secondLevel();
-      thirdLevel();
-      updateLevel();
-      handleCardFlip();
-      updateTimerDisplay();
-    },1500)
-  }else if(game.level === 2 && game.cardPairNum === 8){
-    setTimeout(()=>{
-      thirdLevel();
-      updateLevel();
-      handleCardFlip();
-
-      updateTimerDisplay();
-    },1500)
-    
-  }
-
-}
-
-function handleGameOver() {}
-
-
-function levelPass(){
-  if(game.level === 1){
-    handleCardFlip();
-    if(game.cardPairNum === 2)
-    {
-      updateLevel();
-    }
-  }
-  if(game.level ===2){
-    secondLevel();
-  }
-  if(game.level ===3){
-
-  }
-}
-
+  
 function shuffle(){
-  const card = document.querySelectorAll('.card');
-  card.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 36);
-    card.style.order = randomPos;
+  const cards = document.querySelectorAll('.card');
+  cards.forEach(card=>{
+    let random = Math.floor(Math.random()*10);
+    card.style.order = random;
   });
 }
 
-function passGame(){
-  alert('Congratulations, your score is'+ game.scoreDisplay)
-}
 
 /*******************************************
 /     UI update
 /******************************************/
 function updateScore() {
+  let{scoreDisplay} = game;
   game.score += 1;
-  game.scoreDisplay.innerHTML = game.score;
-}
-
-function updateLevel(){
-  game.level += 1;
-  game.levelDisplay.innerHTML = game.level;
+  scoreDisplay.innerHTML = game.score;
 }
 
 function updateTimerDisplay() {
-  //clearInterval(game.timer);
-  let{timer, timerDisplay} = game;
-  clearInterval(game.timerInterval);
-  game.timerInterval = setInterval(() => {
-  
-    if(timer > 0){
-      timer -= 1;
-      timerDisplay.innerHTML = timer;
+  let {timer, timerDisplay,timerInterval} = game;
+  let time = timer;
+  clearInterval(timerInterval);
+  timerInterval = setInterval(function (){
+    if(time > 0){
+      time -= 1;
+      timerDisplay.innerHTML = time;
+    }else{
+    clearInterval(timerInterval);
+    alert('Time out!');
     }
-    //timerDisplay.innerHTML = timer;
-    
-    console.log(timer);
-    //alert('Time out, your score is'+ game.scoreDisplay);
-    //game.startButton.innerHTML = 'New Game';
-  }, 1000);
-  
-  //clearInterval(timer);
+  },1000); 
+}
+
+function updateLevel(){
+  let {levelDisplay} = game;
+  game.level += 1;
+  levelDisplay.innerHTML = game.level;
+}
+
+function resetGame(){
+  game.cardPairNum = 0;
+  game.score = 0;
+  game.scoreDisplay.innerHTML = game.score;
+  game.level = 1;
+  game.levelDisplay.innerHTML = game.level;
+  game.timerInterval= null;
 }
 
 /*******************************************
@@ -302,5 +279,4 @@ function bindStartButton() {}
 
 function unBindCardClick(card) {}
 
-function bindCardClick(){}
-
+function bindCardClick() {}
